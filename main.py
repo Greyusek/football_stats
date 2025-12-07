b461443e0078a7a12917dd0c6ab2fb07c547e86e
Sergey Kryukov <HomeGreyHome@yandex.ru>
2025-12-07 18:01:47 +0300

    winter 2025

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import plotly.express as px
 
 SUMMER2025 = ['2025-05-29', '2025-08-31']
 AUTUMN2025 = ['2025-09-01', '2025-11-30']
+WINTER2025 = ['2025-12-01', '2026-02-28']
 UPK = 'УПК'
 BARISHIHA = 'Барышиха'
 
@@ -334,10 +335,10 @@ if menu == "Барышиха":
                 else:
                     st.write("Нет данных для одного или обоих выбранных игроков.")
 if menu == "УПК":
-    sub_menu = st.sidebar.radio("УПК:", ("Главная", "Топ", "Общая стат.", "Личная стат."))
+    sub_menu = st.sidebar.radio("УПК:", ("Главная", "Топ", "Общая стат.", "Личная стат.", "Архив"))
     if sub_menu == "Главная":
-        st.title('Осень 2025 (УПК)')
-        total_games, total_draws, total_goals, total_assists, total_saves = season_filter(AUTUMN2025, UPK)
+        st.title('Зима 2025 (УПК)')
+        total_games, total_draws, total_goals, total_assists, total_saves = season_filter(WINTER2025, UPK)
         st.subheader(f"Игр сыграно: {total_games} (из них ничьи: {total_draws})")
         st.subheader(f"Голов забито: {total_goals}")
         st.subheader(f"Пасов отдано: {total_assists}")
@@ -346,8 +347,8 @@ if menu == "УПК":
         #st.subheader("Дней без травм: 0")
         st.write("© MitinoSarayTeam")
     elif sub_menu == "Топ":
-        st.title('Осень 2025 (УПК)')
-        players_df = season_filter_players_stats(AUTUMN2025, 2, UPK)
+        st.title('Зима 2025 (УПК)')
+        players_df = season_filter_players_stats(WINTER2025, 2, UPK)
         points_df = players_df[['player_name', 'points']].sort_values(by='points', ascending=False)
         points_df = points_df.rename(columns={'player_name': 'Игрок', 'points': 'Очки'})
         st.subheader('Общий рейтинг')
@@ -373,7 +374,7 @@ if menu == "УПК":
         st.subheader('Количество ничьих')
         st.dataframe(points_df, hide_index=True, height=200, width=400)
     elif sub_menu == "Общая стат.":
-        st.title('Осень 2025 (УПК)')
+        st.title('Зима 2025 (УПК)')
         st.subheader('Статистика игр по датам')
         date_filter = st.date_input("Выберите дату:", value=pd.to_datetime('today'))
         date_filter_str = date_filter.strftime('%d.%m.%y')
@@ -391,9 +392,9 @@ if menu == "УПК":
         filtered_stats_df['Очки'] = filtered_stats_df['Голы'] + filtered_stats_df['Пасы'] + filtered_stats_df['Победы']*2 + filtered_stats_df['Отстоял на ноль'] + filtered_stats_df['Ничьи']
         st.dataframe(filtered_stats_df, use_container_width=True, hide_index=True, height=750)
     elif sub_menu == "Личная стат.":
-        st.title('Осень 2025 (УПК)')
-        players_df = season_filter_players_stats(AUTUMN2025, 2, UPK)
-        stats_df = season_filter_stats(AUTUMN2025, UPK)
+        st.title('Зима 2025 (УПК)')
+        players_df = season_filter_players_stats(WINTER2025, 2, UPK)
+        stats_df = season_filter_stats(WINTER2025, UPK)
 
         player_name = st.selectbox("Выберите игрока:", players_df['player_name'].unique())
         comparison_player_name = st.selectbox("Выберите игрока для сравнения:", players_df['player_name'].unique())
@@ -456,3 +457,113 @@ if menu == "УПК":
             radar_chart(player_val, comparison_val, player_name, comparison_player_name)
         else:
             st.write("Нет данных для одного или обоих выбранных игроков.")
+    elif sub_menu == "Архив":
+        stat_submenu = st.sidebar.radio("Архив данных:", ("Осень 2025"))
+        if stat_submenu == "Осень 2025":
+            st.title('Осень 2025')
+            player_submenu = st.sidebar.radio("Статистика за осень 2025:", ("Главная", "Топ", "Общая стат.", "Личная стат."))
+            if player_submenu == "Главная":
+                total_games, total_draws, total_goals, total_assists, total_saves = season_filter(AUTUMN2025, UPK)
+                st.subheader(f"Игр сыграно: {total_games}")
+                st.subheader(f"Голов забито: {total_goals}")
+                st.subheader(f"Пасов отдано: {total_assists}")
+                st.subheader("Рыжий пришел на футбол")
+                #st.subheader("Дней без травм: 0")
+                st.write("© MitinoSarayTeam")
+            elif player_submenu == "Топ":
+                players_df = season_filter_players_stats(AUTUMN2025, 2, UPK)
+                points_df = players_df[['player_name', 'points']].sort_values(by='points', ascending=False)
+                points_df = points_df.rename(columns={'player_name': 'Игрок', 'points': 'Очки'})
+                st.subheader('Общий рейтинг')
+                st.dataframe(points_df, hide_index=True, height=200, width=400)
+                goals_df = players_df[['player_name', 'goals']].sort_values(by='goals', ascending=False)
+                goals_df = goals_df.rename(columns={'player_name': 'Игрок', 'goals': 'Голы'})
+                st.subheader('Топ бомбардиров')
+                st.dataframe(goals_df, hide_index=True, height=200, width=400)
+                assists_df = players_df[['player_name', 'assists']].sort_values(by='assists', ascending=False)
+                assists_df = assists_df.rename(columns={'player_name': 'Игрок', 'assists': 'Пасы'})
+                st.subheader('Топ раздающих')
+                st.dataframe(assists_df, hide_index=True, height=200, width=400)
+                points_df = players_df[['player_name', 'wins']].sort_values(by='wins', ascending=False)
+                points_df = points_df.rename(columns={'player_name': 'Игрок', 'wins': 'Побед'})
+                st.subheader('Количество побед')
+                st.dataframe(points_df, hide_index=True, height=200, width=400)
+            elif player_submenu == "Общая стат.":
+                st.subheader('Статистика игр по датам')
+                date_filter = st.date_input("Выберите дату:", value=pd.to_datetime('today'))
+                date_filter_str = date_filter.strftime('%d.%m.%y')
+                filtered_stats_df = stats_df[stats_df['date'] == date_filter_str]
+                filtered_stats_df = filtered_stats_df.rename(columns={
+                    'date': 'Число',
+                    'team_number': '№ команды',
+                    'player_name': 'Игрок',
+                    'goals_on_date': 'Голы',
+                    'assists_on_date': 'Пасы',
+                    'saves_on_date': 'Отстоял на ноль',
+                    'wins_on_date': 'Победы',
+                    'draws_on_date': 'Ничьи'
+                })
+                filtered_stats_df['Очки'] = filtered_stats_df['Голы'] + filtered_stats_df['Пасы'] + filtered_stats_df['Победы']*2 + filtered_stats_df['Отстоял на ноль'] + filtered_stats_df['Ничьи']
+                st.dataframe(filtered_stats_df, use_container_width=True, hide_index=True, height=750)
+            elif player_submenu == "Личная стат.":
+                players_df = season_filter_players_stats(AUTUMN2025, 2, UPK)
+                stats_df = season_filter_stats(AUTUMN2025, UPK)
+                player_name = st.selectbox("Выберите игрока:", players_df['player_name'].unique())
+                comparison_player_name = st.selectbox("Выберите игрока для сравнения:", players_df['player_name'].unique())
+                player_stats = stats_df[stats_df['player_name'] == player_name]
+                comparison_player_stats = stats_df[stats_df['player_name'] == comparison_player_name]  
+                if not player_stats.empty and not comparison_player_stats.empty:
+                    player_stats = player_stats.drop(columns=['player_id', 'player_name'])
+                    player_stats = player_stats.rename(columns={
+                        'date': 'Число',
+                        'team_number': '№ команды',
+                        'goals_on_date': 'Голы',
+                        'assists_on_date': 'Пасы',
+                        'saves_on_date': 'Отстоял на ноль',
+                        'wins_on_date': 'Победы',
+                        'draws_on_date': 'Ничьи'
+                    })
+                    player_stats['Число'] = pd.to_datetime(player_stats['Число']).dt.strftime('%d.%m.%Y')
+                
+                    st.subheader(f'Статистика по игроку: {player_name}')
+        
+                    unique_days = player_stats['Число'].nunique()
+
+                    total_goals = player_stats['Голы'].sum()
+                    total_games = player_stats['Победы'].sum()
+                    total_draws = player_stats['Ничьи'].sum()
+                    total_assists = player_stats['Пасы'].sum()
+                    total_saves = player_stats['Отстоял на ноль'].sum()
+
+                    player_val = [total_goals/unique_days, total_assists/unique_days, total_games/unique_days, total_draws/unique_days, total_saves/unique_days]
+        
+                    st.write(f"Побед: {total_games} Ничьих: {total_draws} Голов: {total_goals} Пасов: {total_assists} Отстоял на ноль: {total_saves}")
+                    st.dataframe(player_stats, use_container_width=True, hide_index=True)
+                    comparison_player_stats = comparison_player_stats.drop(columns=['player_id', 'player_name'])
+                    comparison_player_stats = comparison_player_stats.rename(columns={
+                        'date': 'Число',
+                        'team_number': '№ команды',
+                        'goals_on_date': 'Голы',
+                        'assists_on_date': 'Пасы',
+                        'saves_on_date': 'Отстоял на ноль',
+                        'wins_on_date': 'Победы',
+                        'draws_on_date': 'Ничьи'
+                    })
+                    comparison_player_stats['Число'] = pd.to_datetime(comparison_player_stats['Число']).dt.strftime('%d.%m.%Y')
+
+                    comparison_unique_days = comparison_player_stats['Число'].nunique()
+
+                    comparison_total_goals = comparison_player_stats['Голы'].sum()
+                    comparison_total_games = comparison_player_stats['Победы'].sum()
+                    comparison_total_draws = comparison_player_stats['Ничьи'].sum()
+                    comparison_total_assists = comparison_player_stats['Пасы'].sum()
+                    comparison_total_saves = comparison_player_stats['Отстоял на ноль'].sum()
+                    comparison_val = [comparison_total_goals/comparison_unique_days,
+                                    comparison_total_assists/comparison_unique_days,
+                                    comparison_total_games/comparison_unique_days,
+                                    comparison_total_draws/comparison_unique_days,
+                                    comparison_total_saves/comparison_unique_days]
+        
+                    radar_chart(player_val, comparison_val, player_name, comparison_player_name)
+                else:
+                    st.write("Нет данных для одного или обоих выбранных игроков.")
